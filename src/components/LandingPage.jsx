e95f0d9a4e7ffff144c1980aa1a72dcb281449cb
dgarciarivas <dgarciarivas5@gmail.com>
2017-01-21 18:59:08 -0500

    Revert "simple input list"

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import Input from './Input';
 
 import Base from './Base';
 
 export default class LandingPage extends Base{
 	render(){
 		return ( 
-			<div className='row LandingPage'>
-                <h6 className='flex row'>Statics Topics</h6>
-                <h6 className='flex row'>Physics Topics</h6>
-                <h6 className='flex row'>Intro to flight</h6>
-                <Input/>
+			<div className='LandingPage'>
+                <h6>LandingPage</h6>
             </div>
 		);
 	}
